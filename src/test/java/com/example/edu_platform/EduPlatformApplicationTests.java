d49fc3cab373f1d0631e79961023116c7b5fc0de
alex <lehater@mail.ru>
2025-11-08 15:06:13 +0300

    feat: fix ci-cd

--- src/test/java/com/example/edu_platform/EduPlatformApplicationTests.java
+++ src/test/java/com/example/edu_platform/EduPlatformApplicationTests.java
@@ -2,12 +2,32 @@ package com.example.edu_platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class EduPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", postgres::getJdbcUrl);
+        r.add("spring.datasource.username", postgres::getUsername);
+        r.add("spring.datasource.password", postgres::getPassword);
+        r.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
+
+    @Test
+    void contextLoads() { }
 }
